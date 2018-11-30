726a8d402361ecc2a3d805f2f4bb9d28c4091cc1
Pushkar Kathuria <pushkar.kathuria@cognizant.com>
2018-11-30 14:44:30 +0530

    About screen touchups

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,9 +3,14 @@ import Invoice from '../../_assets/images/ar-content.png';
 import Money from '../../_assets/images/money-bullet.png';
 import Wallet from '../../_assets/images/wallet-bullet.png';
 import Handshake from '../../_assets/images/handshake-bullet.png';
+import Sync from '../../_assets/images/sync.png';
+import Netsuite from '../../_assets/images/netsuite-logo.png';
+import Quickbooks from '../../_assets/images/quickbooks-logo.png';
+import Xero from '../../_assets/images/xero-logo.png';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
 
 const listData = [{
     text: 'Reduce manual labor spent on invoicing your customer.',
@@ -17,6 +22,17 @@ const listData = [{
     text: 'Improve your customer relationship with transparency and excellent communication.',
     type: Handshake
 }];
+
+const ClientsList = [{
+    companyText: 'How we work with Xero accounting.',
+    logo: Xero
+}, {
+    companyText: 'How we work with Quickbooks online.',
+    logo: Quickbooks
+}, {
+    companyText: 'Join Netsuite integration wait list.',
+    logo: Netsuite
+},]
 
 class About extends Component {
     render() {
@@ -43,12 +59,37 @@ class About extends Component {
                                     </List>
                                 })
                             }
+                            <Button variant="contained" color="primary" className='solutions-btn'>
+                                Receivables solutions
+                            </Button>
                         </div>
                     </div>
-
                 </div>
                 <div className='about-footer-container'>
+                    <div className='about-footer-info'>
+                        <img src={Sync} alt='Footer sync'/>
+                        <div className='about-footer-info-text-container'>
+                            <div className='about-footer-info-text'>
+                                Don't worry about updating your ledger.
+                            </div>
+                            <div className='about-footer-info-subtext'>
+                                Our two-way sync keeps your general ledger up-to-date and makes reconcilliation easy.
+                            </div>
+                        </div>
+                    </div>
+                    <div className='about-footer-clients'>
+                            {
+                                ClientsList.map((item) => {
+                                    return <div className='company-details'>
+                                        <img src={item.logo} alt='Company images'/>
+                                        <div className='company-text'>
+                                            {item.companyText}
+                                        </div>
+                                    </div>
 
+                                })
+                            }
+                    </div>
                 </div>
             </div>
         );
